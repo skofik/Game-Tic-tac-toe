df6e464352ac727d76ade56290b52553d10c8b76
Daniel Skowron <skofikit@gmail.com>
2019-04-02 17:53:22 +0200

    Add conditions game

--- main.js
+++ main.js
@@ -8,33 +8,190 @@ let numberDraws = 0;
 let circleScore;
 let crossScore;
 let flag = false;
+let tabelEasy = [true, true, false, true, false, false, false, false];
+let tabelMedium = [true, true, true, true, true, true, false, false];
+let tabelHard = [true, true, true, true, true, true, false, true];
+let tabelInvincible = [true, true, true, true, true, true, true, true];
+let level = "Easy"
+
 
 // Implementation
 
+
+// difficult level of the game
+
+function choiceLevel() {
+  const spanElement = [...document.querySelectorAll(".level span")]
+  spanElement.forEach(span => {
+    span.addEventListener("click", (e) => {
+      const changeColorSpan = () => spanElement.forEach(span => span.setAttribute("style", "color: black"))
+      span.setAttribute("style", "color: black")
+      if (e.target.textContent === "Easy") {
+        changeColorSpan()
+        e.target.setAttribute("style", "color:green; text-decoration-line:underline")
+      } else if (e.target.textContent === "Medium") {
+        changeColorSpan()
+        e.target.setAttribute("style", "color:blue; text-decoration-line:underline")
+      } else if (e.target.textContent === "Hard") {
+        changeColorSpan()
+        e.target.setAttribute("style", "color:red; text-decoration-line:underline")
+      } else if (e.target.textContent === "Invincible") {
+        changeColorSpan()
+        e.target.setAttribute("style", "color:purple; text-decoration-line:underline")
+      }
+      return level = e.target.textContent
+    }
+    )
+  })
+}
+
+const difficultLevel = () => {
+  let tabelBoolean;
+  if (level === "Easy") {
+    let index = Math.floor(Math.random() * (tabelEasy.length - 0) + 0);
+    tabelBoolean = tabelEasy
+    tabelEasy.splice(index, 1)
+  } else if (level === "Medium") {
+    let index = Math.floor(Math.random() * (tabelMedium.length - 0) + 0);
+    tabelBoolean = tabelMedium
+    tabelMedium.splice(index, 1)
+
+  } else if (level === "Hard") {
+    let index = Math.floor(Math.random() * (tabelHard.length - 0) + 0);
+    tabelBoolean = tabelHard
+    tabelHard.splice(index, 1)
+  }
+  else if (level === "Invincible") {
+    let index = Math.floor(Math.random() * (tabelInvincible.length - 0) + 0);
+    tabelBoolean = tabelInvincible
+    tabelInvincible.splice(index, 1)
+  }
+  let index = Math.floor(Math.random() * (tabelBoolean.length - 0) + 0);
+  console.log(tabelBoolean)
+  return tabelBoolean[index]
+}
+
+// function responsible for blocking , winning and checking an empty square 
+const aiBlock = (key, squareKey) => {
+  if (key === 1) {
+    let index = tabelDiv.indexOf(
+      document.querySelector(`[data-key="${squareKey}"]`)
+    );
+    tabelDiv.splice(index, 1);
+    imgCircle.push(squareKey);
+    return squareKey;
+  } else if (typeof squareKey === "string" && key === 0) {
+    if (document.querySelector(`[data-key="${squareKey}"] img `).getAttribute("data-score") === "0") {
+      return true;
+    } else {
+      return false;
+    }
+  } else if (squareKey === "w") {
+    return imgCircle.includes(key);
+  } else {
+    return imgCross.includes(key);
+  }
+};
+
 // Choice Computer
-const computerChoice = function() {
+const computerChoice = function () {
   if (flag === true) {
     let key;
-    let showIndex = Math.floor(Math.random() * (tabelDiv.length - 0) + 0);
-    tabelDiv.splice(showIndex, 1).forEach(div => (key = div.dataset.key));
-    let img = document.querySelector(`[data-key="${key}"] img`);
-    img.setAttribute("src", "img/circle.jpg");
-    img.setAttribute("class", "imgCircle");
-    img.setAttribute("data-score", "2");
-    imgCircle.push(key);
-    check(imgCircle);
-    addScore();
-    flag = false;
+    if (imgCross.length === 5) {
+      addScore()
+    } else {
+      // Intelligence AI
+
+      // conditions winning 
+      if (aiBlock(0, "0") && ((aiBlock("1", "w") && aiBlock("2", "w")) || (aiBlock("4", "w") && aiBlock("8", "w")) || (aiBlock("3", "w") && aiBlock("6", "w")))
+      ) {
+        key = aiBlock(1, "0");
+      } else if (aiBlock(0, "1") && ((aiBlock("0", "w") && aiBlock("2", "w")) || (aiBlock("4", "w") && aiBlock("7", "w")))
+      ) {
+        key = aiBlock(1, "1");
+      } else if (aiBlock(0, "2") && ((aiBlock("0", "w") && aiBlock("1", "w")) || (aiBlock("5", "w") && aiBlock("8", "w")) || (aiBlock("4", "w") && aiBlock("6", "w")))
+      ) {
+        key = aiBlock(1, "2");
+      } else if (aiBlock(0, "3") && ((aiBlock("0", "w") && aiBlock("6", "w")) || (aiBlock("4", "w") && aiBlock("5", "w")))
+      ) {
+        key = aiBlock(1, "3");
+      } else if (aiBlock(0, "5") && ((aiBlock("2", "w") && aiBlock("8", "w")) || (aiBlock("3", "w") && aiBlock("4", "w")))
+      ) {
+        key = aiBlock(1, "5");
+      } else if (aiBlock(0, "6") && ((aiBlock("0", "w") && aiBlock("3", "w")) || (aiBlock("2", "w") && aiBlock("4", "w")) || (aiBlock("7", "w") && aiBlock("8", "w")))
+      ) {
+        key = aiBlock(1, "6");
+      } else if (aiBlock(0, "7") && ((aiBlock("1", "w") && aiBlock("4", "w")) || (aiBlock("6", "w") && aiBlock("8", "w")))
+      ) {
+        key = aiBlock(1, "7");
+      } else if (aiBlock(0, "8") && ((aiBlock("2", "w") && aiBlock("5", "w")) || (aiBlock("0", "w") && aiBlock("4", "w")) || (aiBlock("6", "w") && aiBlock("7", "w")))
+      ) {
+        key = aiBlock(1, "8");
+      } else if (aiBlock(0, "4") && ((aiBlock("1", "w") && aiBlock("7", "w")) || (aiBlock("0", "w") && aiBlock("8", "w")) || (aiBlock("6", "w") && aiBlock("2", "w")) || (aiBlock("3", "w") && aiBlock("5", "w")))
+      ) {
+        key = aiBlock(1, "4");
+      }
+
+      // conditions blocking
+      else if ((aiBlock(0, "0") && difficultLevel()) && (((aiBlock("1") && aiBlock("2")) || (aiBlock("4") && aiBlock("8")) || (aiBlock("3") && aiBlock("6"))) || (aiBlock("4")))
+      ) {
+        key = aiBlock(1, "0");
+      } else if ((aiBlock(0, "1") && difficultLevel()) && (((aiBlock("0") && aiBlock("2")) || (aiBlock("4") && aiBlock("7"))) || ((aiBlock("0") && aiBlock("8")) || (aiBlock("2") && aiBlock("6"))))
+      ) {
+        key = aiBlock(1, "1");
+      } else if ((aiBlock(0, "2") && difficultLevel()) && ((aiBlock("0") && aiBlock("1")) || (aiBlock("5") && aiBlock("8")) || (aiBlock("4") && aiBlock("6")))
+      ) {
+        key = aiBlock(1, "2");
+      } else if ((aiBlock(0, "3") && difficultLevel()) && ((aiBlock("0") && aiBlock("6")) || (aiBlock("4") && aiBlock("5")))
+      ) {
+        key = aiBlock(1, "3");
+      } else if ((aiBlock(0, "5") && difficultLevel()) && ((aiBlock("2") && aiBlock("8")) || (aiBlock("3") && aiBlock("4")))
+      ) {
+        key = aiBlock(1, "5");
+      } else if ((aiBlock(0, "6") && difficultLevel()) && ((aiBlock("0") && aiBlock("3")) || (aiBlock("2") && aiBlock("4")) || (aiBlock("7") && aiBlock("8")))
+      ) {
+        key = aiBlock(1, "6");
+      } else if ((aiBlock(0, "7") && difficultLevel()) && ((aiBlock("1") && aiBlock("4")) || (aiBlock("6") && aiBlock("8")) || (aiBlock("0") && aiBlock("8")))
+      ) {
+        key = aiBlock(1, "7");
+      } else if ((aiBlock(0, "8") && difficultLevel()) && ((aiBlock("2") && aiBlock("5")) || (aiBlock("0") && aiBlock("4")) || (aiBlock("6") && aiBlock("7")))
+      ) {
+        key = aiBlock(1, "8");
+      }
+      else if (aiBlock(0, "4") && ((aiBlock("1") && aiBlock("7")) || (aiBlock("0") && aiBlock("8")) || (aiBlock("6") && aiBlock("2")) || (aiBlock("3") && aiBlock("5")) || (aiBlock("0") || aiBlock("1") || aiBlock("2") || aiBlock("6") || aiBlock("7") || aiBlock("8") || aiBlock("3") || aiBlock("5")))
+      ) {
+        key = aiBlock(1, "4");
+      } else {
+        let index = Math.floor(Math.random() * (tabelDiv.length - 0) + 0);
+        let deleteDiv = tabelDiv.splice(index, 1);
+        key = deleteDiv[0].dataset.key
+        imgCircle.push(`${key}`);
+      }
+
+      let img = document.querySelector(`[data-key="${key}"] img`);
+      img.setAttribute("src", "img/circle.jpg");
+      img.setAttribute("class", "imgCircle");
+      img.setAttribute("data-score", "2");
+      check(imgCircle);
+      addScore();
+      flag = false;
+    }
   }
 };
 
 // Choice player
 
-const choiceSquare = function(event) {
+const choiceSquare = function (event) {
   let key = event.target.dataset.key;
   let img = document.querySelector(`[data-key="${key}"] img`);
   let clikDiv = document.querySelector(`[data-key="${key}"]`);
-  if (img.getAttribute("data-score") == 0) {
+
+  tabelEasy = [true, true, false, false, false, false, false, false];
+  tabelMedium = [true, true, true, true, true, false, false, false];
+  tabelHard = [true, true, true, true, true, true, false, true];
+  tabelInvincible = [true, true, true, true, true, true, true, true];
+
+  if ((img !== null && img.getAttribute("data-score")) === "0") {
     flag = true;
     imgCross.push(key);
     let index = tabelDiv.indexOf(clikDiv);
@@ -42,9 +199,9 @@ const choiceSquare = function(event) {
     img.setAttribute("src", "img/cross.jpg");
     img.setAttribute("class", "imgCross");
     img.setAttribute("data-score", "1");
-    setTimeout(computerChoice, 500);
     check(imgCross);
-  } else {
+    computerChoice();
+  } else if (img === null) {
     alert("Please choose an empty square");
   }
 };
@@ -89,17 +246,18 @@ function clear() {
   crossScore = false;
   circleScore = false;
   message.style.display = "none";
+
 }
 
 // Add score to page
-const addScore = function() {
+const addScore = function () {
   const message = document.querySelector(".message");
 
   if (crossScore === true) {
     numberWins++;
     document.querySelector("p span.wins").textContent = ` ${numberWins}`;
     document.querySelector("p span.wins").style.color = "green";
-    message.textContent = "Congratulations you won ";
+    message.innerHTML = `<span> "Congratulations you won " </span> `;
     message.style.display = "block";
     message.style.color = "green";
     setTimeout(clear, 3000);
@@ -107,15 +265,15 @@ const addScore = function() {
     numberLoses++;
     document.querySelector("p span.losses").textContent = ` ${numberLoses}`;
     document.querySelector("p span.losses").style.color = "red";
-    message.textContent = "sorry, you lost";
+    message.innerHTML = `<span>"sorry, you lost"</span>`;
     message.style.display = "block";
     message.style.color = "red";
     setTimeout(clear, 3000);
-  } else if (tabelDiv.length <= 1) {
+  } else if (imgCross.length === 5) {
     numberDraws++;
     document.querySelector("p span.draws").textContent = ` ${numberDraws}`;
     document.querySelector("p span.draws").style.color = "blue";
-    message.textContent = "Congratulations you drew";
+    message.innerHTML = `<span>"Congratulations you drew"</span>`;
     message.style.display = "block";
     message.style.color = "blue";
     setTimeout(clear, 3000);
@@ -124,7 +282,7 @@ const addScore = function() {
 
 // Choice square
 
-const startGame = function() {
+const startGame = function () {
   tabelDiv.forEach(div => {
     div.addEventListener("click", choiceSquare);
   });
@@ -140,4 +298,5 @@ function showBoard() {
   startGame();
 }
 
+choiceLevel()
 buttonStart.addEventListener("click", showBoard);
